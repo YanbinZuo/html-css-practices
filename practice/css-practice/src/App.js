e63a589349ec724da73929d26a2d5bfc08dd79eb
Yanbin Zuo <jimmyzuo123456@gmail.com>
2024-02-08 09:55:18 -0800

    clone youtube with React init

---
+++ practice/css-practice/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Main from "./Main";
+import SideBar from "./SideBar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SideBar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
