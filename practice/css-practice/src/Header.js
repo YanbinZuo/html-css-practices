ba9dbf9062b666de769553ee9924d0fca128841e
Yanbin Zuo <jimmyzuo123456@gmail.com>
2024-02-08 10:39:27 -0800

    fixed defect that images need located in public folder

--- practice/css-practice/src/Header.js
+++ practice/css-practice/src/Header.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div>
+        <img
+          src="./icons/explore.svg"
+          alt="pic"
+        />
+       
+      </div>
+      <div>middle</div>
+      <div>right</div>
+    </header>
+  );
 }
--- practice/css-practice/src/index.js
+++ practice/css-practice/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/general.css";
+import "./styles/header.css";
+import "./styles/main.css";
+import "./styles/sidebar.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
