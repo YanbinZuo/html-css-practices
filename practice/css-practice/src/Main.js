b657a8d28143f003462dc1474e67a73dd0812bd9
Yanbin Zuo <jimmyzuo123456@gmail.com>
2024-02-12 11:47:29 -0800

    add main contents

--- practice/css-practice/src/Main.js
+++ practice/css-practice/src/Main.js
@@ -11,23 +11,426 @@ export default function Main() {
               target="_blank"
               rel="noreferrer"
             >
-              <img />
+              <img
+                className="thumbnail"
+                src="thumbnails/thumbnail-1.webp"
+                alt="thumbnail-1"
+              />
             </a>
-            <div>14:20</div>
+            <div className="video-time">14:20</div>
           </div>
-          <div className="info-container">
-            <div className="profile">
-              <img />
+          <div className="video-info-grid">
+            <div className="channel-picture">
+              <div className="profile-picture-container">
+                <a
+                  href="https://www.youtube.com/c/mkbhd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="profile-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                </a>
+                <div className="channel-tooltip">
+                  <img
+                    class="channel-tooltip-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                  <div class="channel-tooltip-infos">
+                    <p class="channel-tooltip-name">Marques Brownlee</p>
+                    <p class="channel-tooltip-stats">15M subscribers</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="info">
-              <p>Talking ....</p>
-              <p>Marques Brownlee</p>
-              <p>3.3M views . 6 months ago</p>
+            <div className="video-info">
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-title">
+                  Talking Tech and AI with Google CEO Sundar Pichai!
+                </p>
+              </a>
+              <a
+                href="https://www.youtube.com/c/mkbhd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-author">Marques Brownlee</p>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-stats">3.4M views &#183; 6 months ago</p>
+              </a>
             </div>
           </div>
         </div>
 
-        <div></div>
+
+
+
+
+
+
+
+
+        
+        <div className="video-preview">
+          <div className="thumbnail-row">
+            <a
+              href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="thumbnail"
+                src="thumbnails/thumbnail-1.webp"
+                alt="thumbnail-1"
+              />
+            </a>
+            <div className="video-time">14:20</div>
+          </div>
+          <div className="video-info-grid">
+            <div className="channel-picture">
+              <div className="profile-picture-container">
+                <a
+                  href="https://www.youtube.com/c/mkbhd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="profile-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                </a>
+                <div className="channel-tooltip">
+                  <img
+                    class="channel-tooltip-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                  <div class="channel-tooltip-infos">
+                    <p class="channel-tooltip-name">Marques Brownlee</p>
+                    <p class="channel-tooltip-stats">15M subscribers</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="video-info">
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-title">
+                  Talking Tech and AI with Google CEO Sundar Pichai!
+                </p>
+              </a>
+              <a
+                href="https://www.youtube.com/c/mkbhd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-author">Marques Brownlee</p>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-stats">3.4M views &#183; 6 months ago</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="video-preview">
+          <div className="thumbnail-row">
+            <a
+              href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="thumbnail"
+                src="thumbnails/thumbnail-1.webp"
+                alt="thumbnail-1"
+              />
+            </a>
+            <div className="video-time">14:20</div>
+          </div>
+          <div className="video-info-grid">
+            <div className="channel-picture">
+              <div className="profile-picture-container">
+                <a
+                  href="https://www.youtube.com/c/mkbhd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="profile-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                </a>
+                <div className="channel-tooltip">
+                  <img
+                    class="channel-tooltip-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                  <div class="channel-tooltip-infos">
+                    <p class="channel-tooltip-name">Marques Brownlee</p>
+                    <p class="channel-tooltip-stats">15M subscribers</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="video-info">
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-title">
+                  Talking Tech and AI with Google CEO Sundar Pichai!
+                </p>
+              </a>
+              <a
+                href="https://www.youtube.com/c/mkbhd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-author">Marques Brownlee</p>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-stats">3.4M views &#183; 6 months ago</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="video-preview">
+          <div className="thumbnail-row">
+            <a
+              href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="thumbnail"
+                src="thumbnails/thumbnail-1.webp"
+                alt="thumbnail-1"
+              />
+            </a>
+            <div className="video-time">14:20</div>
+          </div>
+          <div className="video-info-grid">
+            <div className="channel-picture">
+              <div className="profile-picture-container">
+                <a
+                  href="https://www.youtube.com/c/mkbhd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="profile-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                </a>
+                <div className="channel-tooltip">
+                  <img
+                    class="channel-tooltip-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                  <div class="channel-tooltip-infos">
+                    <p class="channel-tooltip-name">Marques Brownlee</p>
+                    <p class="channel-tooltip-stats">15M subscribers</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="video-info">
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-title">
+                  Talking Tech and AI with Google CEO Sundar Pichai!
+                </p>
+              </a>
+              <a
+                href="https://www.youtube.com/c/mkbhd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-author">Marques Brownlee</p>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-stats">3.4M views &#183; 6 months ago</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="video-preview">
+          <div className="thumbnail-row">
+            <a
+              href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="thumbnail"
+                src="thumbnails/thumbnail-1.webp"
+                alt="thumbnail-1"
+              />
+            </a>
+            <div className="video-time">14:20</div>
+          </div>
+          <div className="video-info-grid">
+            <div className="channel-picture">
+              <div className="profile-picture-container">
+                <a
+                  href="https://www.youtube.com/c/mkbhd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="profile-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                </a>
+                <div className="channel-tooltip">
+                  <img
+                    class="channel-tooltip-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                  <div class="channel-tooltip-infos">
+                    <p class="channel-tooltip-name">Marques Brownlee</p>
+                    <p class="channel-tooltip-stats">15M subscribers</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="video-info">
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-title">
+                  Talking Tech and AI with Google CEO Sundar Pichai!
+                </p>
+              </a>
+              <a
+                href="https://www.youtube.com/c/mkbhd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-author">Marques Brownlee</p>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-stats">3.4M views &#183; 6 months ago</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="video-preview">
+          <div className="thumbnail-row">
+            <a
+              href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="thumbnail"
+                src="thumbnails/thumbnail-1.webp"
+                alt="thumbnail-1"
+              />
+            </a>
+            <div className="video-time">14:20</div>
+          </div>
+          <div className="video-info-grid">
+            <div className="channel-picture">
+              <div className="profile-picture-container">
+                <a
+                  href="https://www.youtube.com/c/mkbhd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="profile-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                </a>
+                <div className="channel-tooltip">
+                  <img
+                    class="channel-tooltip-picture"
+                    src="./channel-pictures/channel-1.jpeg"
+                    alt="channel-1"
+                  />
+                  <div class="channel-tooltip-infos">
+                    <p class="channel-tooltip-name">Marques Brownlee</p>
+                    <p class="channel-tooltip-stats">15M subscribers</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="video-info">
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-title">
+                  Talking Tech and AI with Google CEO Sundar Pichai!
+                </p>
+              </a>
+              <a
+                href="https://www.youtube.com/c/mkbhd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-author">Marques Brownlee</p>
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=n2RNcPRtAiY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p class="video-stats">3.4M views &#183; 6 months ago</p>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+
       </section>
     </main>
   );
